9f3d60a4de710cfe7ca139681e7f8e4829a2f2de
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2021-08-22 14:16:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EpidemicModeling
+  A very simple epidemic model that shows that the epidemic has a peak and a decrease in infectiousness
---
+++ model.py
@@ -0,0 +1,42 @@
+import time
+import random
+import matplotlib.pyplot as plt
+
+x_points = []
+y_points = []
+recovered = 0
+vulnerable = 5000
+population = recovered + vulnerable
+infectivity_index = 1
+infected = 1
+day = 0
+
+while infected <= population:
+    population = recovered + vulnerable
+    print(f'День: {day}, количество инфицированных: {infected}, количество переболевших: {recovered}, количество '
+          f'уязвимых к вирусу {vulnerable}, популяция человечества: {population}')
+    x_points.append(infected)
+    y_points.append(day)
+    day += 1
+    can_infect = vulnerable
+    if recovered == population-1:
+        print(f'День: {day}, количество инфицированных: 0, количество переболевших: {population}, количество '
+              f'уязвимых к вирусу 0, популяция человечества: {population}, победа человечества получается')
+        break
+    infected += infected * infectivity_index
+    if infected > can_infect:
+        infected = vulnerable
+    if (day != 0) and (day % 3) == 0:
+        people_will_be_recovered = infected//random.randint(2,3)
+        recovered += people_will_be_recovered
+        infected -= people_will_be_recovered
+        vulnerable -= people_will_be_recovered
+
+
+plt.title("Эпидемия")
+plt.xlabel("День")
+plt.ylabel("Инфицированные")
+plt.grid()
+plt.plot(y_points, x_points)
+plt.show()
+
